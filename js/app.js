a19ee262e5230a05b02249e8e9ab6234c30e365f
shakil <shakil.hossain450s@gmail.com>
2024-09-25 18:42:54 +0600

    categories loaded by api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log("connected");
+const loadCategories = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
+    const data = await res.json();
+    // console.log(data.data.news_category);
+    const categories = data.data.news_category;
+    displayCategories(categories);
+}
+
+const displayCategories = categories => {
+    const categoriesContainer = document.querySelector("#categories-container");
+    categories.forEach(category => {
+        const {category_name} = category;
+
+        const categoryButton = document.createElement("button");
+        categoryButton.classList.add("hover:text-[#5D5FEF]");
+        categoryButton.innerText = category_name;
+        
+        categoriesContainer.appendChild(categoryButton);
+    });
+    
+}
+loadCategories();
